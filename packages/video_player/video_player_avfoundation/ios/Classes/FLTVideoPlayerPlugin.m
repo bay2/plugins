d4022ed158334d2b438970dc3398a3c8a11a43ea
Sim Tsai <sim@Mac-Pro.local>
2022-07-12 16:01:20 +0800

    IOS客户端预览视频的过程中，将进度条拉到00:00，画面卡住

--- packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
@@ -384,9 +384,10 @@ NS_INLINE CGFloat radiansToDegrees(CGFloat radians) {
   // TODO(stuartmorgan): Update this to use completionHandler: to only return
   // once the seek operation is complete once the Pigeon API is updated to a
   // version that handles async calls.
-  [_player seekToTime:CMTimeMake(location, 1000)
-      toleranceBefore:kCMTimeZero
-       toleranceAfter:kCMTimeZero];
+    [_player seekToTime:CMTimeMake(location, 1000)];
+//  [_player seekToTime:CMTimeMake(location, 1000)
+//      toleranceBefore:kCMTimeZero
+//       toleranceAfter:kCMTimeZero];
 }
 
 - (void)setIsLooping:(BOOL)isLooping {
